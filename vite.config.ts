9a772e4b775bf70c9f2ff8145905fb022e4f597c
sury <2171924053@qq.com>
2026-03-02 16:55:56 +0800

    a

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
-    const base = mode === 'production' ? '/' : '/';
+    const base = mode === 'production' ? '/Siyue/' : '/';
     return {
       base: base,
       server: {
